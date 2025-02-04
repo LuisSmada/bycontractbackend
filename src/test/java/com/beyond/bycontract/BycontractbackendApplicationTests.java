9c983850dcd6aac2032e32c4fd91b3d2b77624ca
AYO NKONGHO Luis Adams <LuisAdams.AYONKONGHO@3ds.com>
2025-02-04 15:05:43 +0100

    Base files structure of the backend

---
+++ src/test/java/com/beyond/bycontract/BycontractbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.bycontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BycontractbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
